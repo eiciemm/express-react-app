8a4603f1f258c74f1226c9242a26a492c49096bb
松井永 <matsuihisashi@HMatsui.local>
2021-05-30 12:51:05 +0900

    不要なRoute削除

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 3001
 
-var upcaseRouter = require('./routes/upcase');
-var calculateRouter = require('./routes/calculate');
 var memoRouter = require('./routes/memo');
 
 app.use(bodyParser.urlencoded({
@@ -12,8 +10,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use('/upcase', upcaseRouter);
-app.use('/calculate', calculateRouter);
 app.use('/memo', memoRouter);
 
 app.listen(port, () => {
