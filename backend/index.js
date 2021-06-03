dc841f67d6861744090b153f34151226d336711c
松井永 <matsuihisashi@HMatsui.local>
2021-06-04 00:47:59 +0900

    cors

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,23 @@ const port = process.env.PORT || 3001
 
 var memoRouter = require('./routes/memo');
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+app.use(allowCrossDomain)
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
